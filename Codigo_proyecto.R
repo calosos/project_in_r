3250e7e994502dca500c887aecf6d7c87ee7d0cd
Aldair Ortiz <caos-97@hotmail.com>
2022-03-07 16:40:22 -0600

    several changes

--- Codigo_proyecto.R
+++ Codigo_proyecto.R
@@ -10,10 +10,41 @@ View(datos_soccer_20_21)
 goles_casa_visita <- data.frame(datos_soccer_20_21$FTHG, datos_soccer_20_21$FTAG)
 View(goles_casa_visita)
 names(goles_casa_visita)[1]<-"FTHG" ;names(goles_casa_visita)[2]<-"FTAG"
+write.csv(goles_casa_visita,"Datos_goles_casa_visita.csv")
 
 #Se calcula la frecuancia absoluta de FTHG
-table(goles_casa_visita$FTHG)
+(frec_abs_FTHG <- table(goles_casa_visita$FTHG))
+(frec_abs_FTHG_df <- data.frame(frec_abs_FTHG))
+#Se calcula la frecuencia relativa de FTHG
+(frec_relativa_FTHG <- prop.table(frec_abs_FTHG))
+(frec_relativa_FTHG_df <- data.frame(frec_relativa_FTHG))
 
-table(goles_casa_visita$FTAG)
-.
+FTHG_abs_rel_df <- cbind(frec_abs_FTHG_df,frec_relativa_FTHG_df$Freq)
+names(FTHG_abs_rel_df)[1]<-"#Goles" 
+names(FTHG_abs_rel_df)[2]<-"Frecuencia Absoluta";
+names(FTHG_abs_rel_df)[3]<-"Frecuencia Relativa"
+
+#Probabilidad Marginasl FTHG
+(numero_partidos <- length(goles_casa_visita$FTHG))
+(prob_marg_FTHG <- FTHG_abs_rel_df$`Frecuencia Absoluta`/ numero_partidos)
+FTHG_abs_rel_probmarg_df<-cbind(FTHG_abs_rel_df, prob_marg_FTHG)
+names(FTHG_abs_rel_probmarg_df)[4]<-"Probabilidad Marginal"
+
+
+###################################################
+
+#Se calcula la frecuancia absoluta de FTAG
+(frec_abs_FTAG <- table(goles_casa_visita$FTAG))
+(frec_abs_FTAG_df <- data.frame(frec_abs_FTAG ))
+
+#Se calcula la frecuencia relatica de FTHG
+(frec_relativa_FTAG<- prop.table(frec_abs_FTAG))
+(frec_relativa_FTAG_df <- data.frame(frec_relativa_FTAG))
+
+FTAG_abs_rel_df <- cbind(frec_abs_FTAG_df,frec_relativa_FTAG_df$Freq)
+names(FTAG_abs_rel_df)[1]<-"#Goles" 
+names(FTAG_abs_rel_df)[2]<-"Frecuencia Absoluta";
+names(FTAG_abs_rel_df)[3]<-"Frecuencia Relativa"  
+
+
 
