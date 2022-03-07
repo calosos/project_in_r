e879e3e7d3f64415462ab633f53c91477d11b163
Carlos Ortiz <aldairortizsuarez@gmail.com>
2022-03-07 02:27:45 -0600

    add several change

--- Codigo_proyecto.R
+++ Codigo_proyecto.R
@@ -1,5 +1,5 @@
 #Fijar Ruta para obtener datos
-setwd("C:/Proyecto_entregable_programación_y_estadistica_con_r")
+setwd("E:/project_in_r")
 getwd()
 
 #Obtener y guardar datos
@@ -12,8 +12,27 @@ View(goles_casa_visita)
 names(goles_casa_visita)[1]<-"FTHG" ;names(goles_casa_visita)[2]<-"FTAG"
 
 #Se calcula la frecuancia absoluta de FTHG
-table(goles_casa_visita$FTHG)
+(FTHG_frec_absoluta <- table(goles_casa_visita$FTHG))
+FTHG_frec_relativa <- prop.table(FTHG_frec_absoluta)
+FTHG_frec_absoluta_df <- data.frame( table(goles_casa_visita$FTHG))
 
-table(goles_casa_visita$FTAG)
-.
+#Se calcula la frecuancia absoluta de FTAG
+FTAG_frec_absoluta <- table(goles_casa_visita$FTAG)
+FTAG_frec_relativa <- prop.table(FTAG_frec_absoluta)
+FTAG_frec_absoluta_df <- data.frame( table(goles_casa_visita$FTAG))
 
+#Probabilidad marginal FTHG
+total_partidos <-length(goles_casa_visita$FTHG)
+
+(prob_marg_FTHG <- FTHG_frec_absoluta_df$Freq/ total_partidos*100)
+
+
+#Probabilidad marginal FTAG
+(prob_marg_FTAG <- FTAG_frec_absoluta_df$Freq/ total_partidos*100)
+
+
+###
+View(goles_casa_visita)
+table(goles_casa_visita$FTHG, goles_casa_visita$FTAG )
+
+length(goles_casa_visita$FTHG)
